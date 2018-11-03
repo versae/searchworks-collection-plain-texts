3b76b0a268b7e8a4f46af1ed8f18560c8fdcb873
Javier de la Rosa <versae@gmail.com>
2018-11-02 22:21:06 -0700

    Updates

---
+++ precache-manifest.42ff5ba4fb782083ff63021c022988f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e0f9ac88ea2652e4bd3e0e8afa5545d",
+    "url": "/searchworks-collection-plain-texts/static/media/logo.1e0f9ac8.svg"
+  },
+  {
+    "revision": "89fff20d8a0ea51a6a35",
+    "url": "/searchworks-collection-plain-texts/static/js/runtime~main.89fff20d.js"
+  },
+  {
+    "revision": "56b6e75caa36a9b9b9b3",
+    "url": "/searchworks-collection-plain-texts/static/js/main.56b6e75c.chunk.js"
+  },
+  {
+    "revision": "10d21a16efda7a9c2094",
+    "url": "/searchworks-collection-plain-texts/static/js/1.10d21a16.chunk.js"
+  },
+  {
+    "revision": "56b6e75caa36a9b9b9b3",
+    "url": "/searchworks-collection-plain-texts/static/css/main.599a89db.chunk.css"
+  },
+  {
+    "revision": "abcc6f5fc90539ed19189635cff873ab",
+    "url": "/searchworks-collection-plain-texts/index.html"
+  }
+];
