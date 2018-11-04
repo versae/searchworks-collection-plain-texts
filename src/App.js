8fe1253595d8c846547958763487718e9378b16e
Javier de la Rosa <versae@gmail.com>
2018-11-04 15:48:41 -0800

    Improving progress messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { parse as URLParse } from "url";
+import { parse as URLParse } from 'url';
 import React, { Component } from 'react';
 
 import PdfJsLib from 'pdfjs-dist';
@@ -11,6 +11,8 @@ import Manifesto from 'manifesto.js';
 import logo from './logo.svg';
 import './App.css';
 
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
+const TEST_COLLECTION = 'https://purl.stanford.edu/jt466yc7169';
 PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js';
 
 const getPDFsFromIIIFManifest = uri => fetch(uri, { method: 'GET' })
@@ -27,15 +29,20 @@ const getPDFsFromIIIFManifest = uri => fetch(uri, { method: 'GET' })
     )
   ).then(flat);
 
+const fileSize = size => {
+    const e = (Math.log(size) / Math.log(1e3)) | 0;
+    return +(size / Math.pow(1e3, e)).toFixed(2) + ' ' + ('kMGTPEZY'[e - 1] || '') + 'B';
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      docs: [],
+      zips: {},
+      status: {},
       progress: null
     }
-    this.zip = new JSZip();
-    this.maxDownloadFiles = 5;  // max is limited to 100 by searchworks
+    this.maxDownloadFiles = 10;  // max is limited to 100 by searchworks
     this.input = React.createRef();
     this.addTextToZip = this.addTextToZip.bind(this);
     this.downloadZip = this.downloadZip.bind(this);
@@ -44,15 +51,26 @@ class App extends Component {
   }
 
   handleTryOnClick() {
-    this.input.current.value = "https://purl.stanford.edu/jt466yc7169";
+    this.input.current.value = TEST_COLLECTION;
   }
 
   handleDownloadOnClick() {
     const url = this.input.current.value;
     const { hostname, path } = URLParse(url);
-    if (hostname === "purl.stanford.edu") {
-      const proxy = 'https://cors-anywhere.herokuapp.com/';
-      const feed = `${proxy}https://searchworks.stanford.edu/catalog.json?f%5Bcollection%5D%5B%5D=${path.slice(1)}&per_page=${this.maxDownloadFiles}`;
+    if (['purl.stanford.edu', 'searchworks.stanford.edu'].includes(hostname)) {
+      this.setState({progress: 0, status: {[[url]]: "Downloading collection..."}});
+      let collection;
+      let feed;
+      if (hostname === 'purl.stanford.edu') {
+        collection = path.slice(1);
+        feed = `${PROXY}https://searchworks.stanford.edu/catalog.json?f%5Bcollection%5D%5B%5D=${collection}&per_page=${this.maxDownloadFiles}`;
+      } else if (url.indexOf("searchworks.stanford.edu/catalog.atom") >= 0) {
+        collection = url.replace("catalog.atom", "catalog.json");
+        feed = `${PROXY}${collection}&per_page=${this.maxDownloadFiles}`;
+      } else if (url.indexOf('searchworks.stanford.edu/catalog?') >= 0) {
+        collection = url.replace('catalog?', 'catalog.json?');
+        feed = `${PROXY}${collection}&per_page=${this.maxDownloadFiles}`;
+      }
       fetch(feed, { method: 'GET' })
         .then(response => {
           this.setState({progress: 0});
@@ -62,80 +80,115 @@ class App extends Component {
           return jsonFeed.response.docs.flatMap(doc => `${doc.url_fulltext}/iiif/manifest`);
         })
         .then(manifests => {
-          console.log(manifests)
+          this.setState({status: {[[url]]: "Downloading contents..."}});
           manifests.forEach(manifest => {
-            this.retrieveManifest(manifest);
+            this.retrieveManifest(collection, manifest);
           })
         });
+      this.input.current.value = "";
     }
   }
 
-  downloadZip() {
-    console.log(this.state.docs);
-    this.zip.generateAsync({type:"blob"}).then(function (blob) {
-        saveAs(blob, "collection.zip");
+  downloadZip(label) {
+    this.state.zips[label].generateAsync({type: 'blob'}).then(function (blob) {
+        saveAs(blob, `${label}.zip`);
     }, function (err) {
         console.log(err);
     });
   }
 
-  addTextToZip(label, texts) {
-    console.log(`${label}\n-------------------`)
+  addTextToZip(label, filename, texts) {
+    this.setState({
+      zips: {[[label]]: new JSZip(), ...this.state.zips},
+      status: {...this.state.status, [[filename]]: "Adding to zip..."}
+    });
     Promise.all(texts.map(promise => promise.catch(err => err)))
     .then(textChunks => {
-      const text = textChunks.join(" ");
+      const text = textChunks.join(' ');
       this.setState({
-        docs: [{label, text}, ...this.state.docs],
         progress: this.state.progress + 1
       });
-      this.zip.file(`${label}.txt`, text);
+      this.state.zips[label].file(`${filename}.txt`, text);
+      this.setState({
+        status: {...this.state.status, [[filename]]: "Done"}
+      });
       if (this.state.progress === this.maxDownloadFiles) {
-        this.downloadZip();
+        this.downloadZip(label);
+        this.setState({
+          progress: null,
+          status: {},
+        })
       }
     })
-    .catch(e => console.log(e));
+    .catch(err => console.log(err));
   }
 
-  retrieveManifest(manifestUri) {
+  retrieveManifest(label, manifestUri) {
     getPDFsFromIIIFManifest(manifestUri).then(pdfFiles => {
-      pdfFiles.map(pdfFile => {
-        PdfJsLib.getDocument(pdfFile.uri).then((pdf) => {
-          const texts = [...Array(pdf._pdfInfo.numPages).keys()].map(pageNumber => (
-            pdf.getPage(pageNumber + 1).then(pdfPage => (
+      return pdfFiles.map(pdfFile => {
+        const pdfDoc = PdfJsLib.getDocument(pdfFile.uri)
+        pdfDoc.onProgress = ({loaded, total}) => {
+          this.setState({status: {...this.state.status, [[pdfFile.label]]: fileSize(loaded)}})
+        }
+        return pdfDoc.then((pdf) => {
+          const texts = [...Array(pdf._pdfInfo.numPages).keys()].map(pageNumber => {
+            this.setState({status: {...this.state.status, [[pdfFile.label]]: `Extractig page ${pageNumber + 1}`}})
+            return pdf.getPage(pageNumber + 1).then(pdfPage => (
               pdfPage.getTextContent().then(textContent => (
-                textContent.items.map(text => text.str).join(" ")
+                textContent.items.map(text => text.str).join(' ')
               ))
             ))
-          ));
-          this.addTextToZip(pdfFile.label, texts);
+          });
+          this.addTextToZip(label, pdfFile.label, texts);
         });
       })
+    }).then(files => {
+      if (files.length === 0) {
+        this.setState({status: {...this.state.status, [[manifestUri]]: "No PDFs found"}})
+      }
     })
   }
 
   render() {
-    const progress = (this.state.progress !== null && this.state.progress !== this.maxDownloadFiles ) ? (
+    const progress = (this.state.progress !== null && this.state.progress !== this.maxDownloadFiles) ? (
       <progress min={0} max={this.maxDownloadFiles} value={this.state.progress} />
     ) : ('');
-
+    const status = Object.keys(
+      this.state.status
+    ).map((item, idx) => (
+      [<dt key={`dd${idx}`}>{item}</dt>, <dd>{this.state.status[item]}</dd>]
+    ));
+    const downloads = Object.keys(this.state.zips).map(zip => (
+      <li>Download <button href="#" onClick={() => this.downloadZip(zip)}>{zip}.zip</button></li>
+    ));
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            To download the available plain texts of a collection,
-            enter its URL and click on Download (only the first {this.maxDownloadFiles} items will be retrieved)
+            To download the available plain texts of a catalog collection containing PDF files,
+            <br />
+            enter its URL and click on Download (only the first ~{this.maxDownloadFiles} items will be retrieved)
           </p>
           <p>
-            <input ref={this.input} />
+            <input type="text" ref={this.input} placeholder={TEST_COLLECTION} />
             <input type="submit" onClick={this.handleDownloadOnClick} value="Download" />
+            <br />
+            <small>
+              Try the <button href="#" onClick={this.handleTryOnClick}>
+                Jarndyce Single-Volume Nineteenth-Century Novel Collection, 1823-1914
+              </button>.
+            </small>
           </p>
-          <small>
-            Try the <a href="#" onClick={this.handleTryOnClick}>
-              Jarndyce Single-Volume Nineteenth-Century Novel Collection, 1823-1914
-            </a>.
-          </small>
-          {progress}
+          <div>
+            {progress}
+            <dl>
+              {status}
+            </dl>
+            <ul>
+              {downloads}
+            </ul>
+          </div>
         </header>
       </div>
     );
